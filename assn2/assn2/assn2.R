48db57d811104519c9e851efa583a44b4f3fcc90
lucetre <lucetre@naver.com>
2021-07-05 21:21:53 +0900

    Heelo

--- assn2/assn2/assn2.R
+++ assn2/assn2/assn2.R
@@ -14,18 +14,18 @@ cor(cdc$weight, cdc$wtdesire)
 # ex4
 wdiff <- cdc$wtdesire - cdc$weight
 summary(wdiff)
-plot(wdiff, main="Desired weight change (lb)")
-# boxplot(wdiff, horizontal=T)
+plot(wdiff,    main="Desired weight change (lb)")
+boxplot(wdiff, main="Desired weight change (lb)")
 
 # ex5
 hist(cdc$age, breaks=50,  main="Age Hist w. 50 breaks")
 hist(cdc$age, breaks=100, main="Age Hist w. 100 breaks")
 
 # ex6
-plot(cdc$height, cdc$width, 
-     main="correlation of height and width",
+plot(cdc$height, cdc$weight, 
+     main="correlation of height and weight",
      xlab="height", ylab="weight")
-cor(cdc$weight, cdc$wtdesire)
+cor(cdc$height, cdc$weight)
 
 # ex7
 bodydims <- read.csv("bodydims.csv", header=T)
@@ -38,10 +38,7 @@ summary(bodydims$elb.di)
 male   = bodydims[bodydims$sex == 1, ]
 female = bodydims[bodydims$sex == 0, ]
 
-ylim = c(0,100)
-xlim = c(140,200)
-
-hist(male$hgt, ylim=ylim, xlim=xlim,
+hist(male$hgt, ylim=c(0,100), xlim=c(140,200),
      main="height of males")
-hist(female$hgt, ylim=ylim, xlim=xlim, 
+hist(female$hgt, ylim=c(0,100), xlim=c(140,200), 
      main="height of females")
